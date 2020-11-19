8a4a292f8923e8702e9f508792247c8f58e01f70
mjwills-inf <mjwills@live.co.uk>
2020-11-19 17:04:10 +0000

    .

--- src/ components/Nav.js
+++ src/ components/Nav.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 
 export default function Nav() {
   return (
     <nav>
-      Nav bar
+      <h3>Logo</h3>
+        <ul className="nav-links">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/shop">
+            <li>Shop</li>
+          </Link>
+        </ul>
+      <Link to="/cart">
+        <span>
+          Cart
+        </span>
+      </Link>
     </nav>    
   )
 }
--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import Nav from './ components/Nav'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Nav from './ components/Nav';
+import Home from './ components/Home/Home';
+import Shop from './ components/Shop//Shop';
+import Cart from './ components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-     <h1>App</h1>
-    </div>
+    <Router> 
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/cart" exact component={Cart}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
