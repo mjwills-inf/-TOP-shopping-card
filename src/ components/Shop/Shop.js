db10fc9517d3f900b7821b981638a9d4e44f1d09
mjwills-inf <mjwills@live.co.uk>
2020-11-20 16:16:43 +0000

    .

--- src/ components/Shop/Shop.js
+++ src/ components/Shop/Shop.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Shop() {
+
+  useEffect(() => {
+    fetchShopData()
+  }, [])
+
+  const fetchShopData = async () => {
+    const data = await fetch('')
+  }
+
   return (
     <div>
       Shop
