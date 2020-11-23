f6fbc06cfc61461be75717d68750b6f9b608a1d5
mjwills-inf <mjwills@live.co.uk>
2020-11-23 12:52:28 +0000

    .

--- src/ components/Shop/Shop.js
+++ src/ components/Shop/Shop.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react'
 
+import Sidebar from './Sidebar'
+
 export default function Shop() {
+
+  const [items, setItems] = useState([])
 
   useEffect(() => {
     fetchShopData()
   }, [])
 
   const fetchShopData = async () => {
-    const data = await fetch('')
+    const data = await fetch('https://fakestoreapi.com/products/')
+      .then(response=>response.json(), reason =>console.log(reason))
+    console.log(data)
+    setItems(data)
   }
 
   return (
-    <div>
-      Shop
+    <div className="shop">
+      <Sidebar />
+      <div className="items">
+        {items.map(item => (
+          <h4>{item.title}</h4>
+        ))}
+      </div>
     </div>
   )
 }
