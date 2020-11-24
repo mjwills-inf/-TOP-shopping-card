365fcce766d4c951559a7f6fc71b32f7a1fdc79c
mjwills-inf <mjwills@live.co.uk>
2020-11-24 11:58:07 +0000

    .

--- src/ components/ItemDetail.js
+++ src/ components/ItemDetail.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function ItemDetail() {
+export default function ItemDetail(match) {
+
+  const [item, setItem] = useState()
+
+  useEffect(() => {
+    fetchItemData()
+    console.log("match", match)
+  }, [])
+
+  const fetchItemData = async () => {
+    const data = await fetch(`https://fakestoreapi.com/products/${match.match.params.id}`)
+      .then(response=>response.json(), reason =>console.log(reason))
+    setItem(data)
+    console.log(data)
+    console.log(item)
+  }
+
   return (
     <div>
-      ItemDetail
+      Item Details
     </div>
   )
 }
