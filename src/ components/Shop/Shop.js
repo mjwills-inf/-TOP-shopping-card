8e8b348241d3b2b27ac321a559000fc7d77a131e
mjwills-inf <mjwills@live.co.uk>
2020-11-25 16:14:31 +0000

    .

--- src/ components/Shop/Shop.js
+++ src/ components/Shop/Shop.js
@@ -1,32 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Sidebar from './Sidebar'
+import Cart from '../Cart/Cart'
+import ItemDetail from '../ItemDetail';
 
-export default function Shop() {
+export default function Shop(props) {
 
-  const [items, setItems] = useState([])
-
-  useEffect(() => {
-    fetchShopData()
-  }, [])
-
-  const fetchShopData = async () => {
-    const data = await fetch('https://fakestoreapi.com/products/')
-      .then(response=>response.json(), reason =>console.log(reason))
-    setItems(data)
-  }
 
   return (
     <div className="shop">
       <Sidebar />
-      <div className="items">
-        {items.map(item => (          
-            <div key={item.id}>
-              <Link to={`/shop/${item.id}`}>{item.title}</Link>
-            </div>
+
+      <div className="shop-main">
+        <Switch>
+        <Route path="/cart" exact>
+          <Cart 
           
-        ))}
+          />   
+        </Route> 
+
+        <Route path="/shop/:id" component={ItemDetail}/>
+        {/* <div className="items">
+          {items.map(item => (          
+              <div key={item.id}>
+                <Link to={`/shop/${item.id}`}>{item.title}</Link>
+              </div>
+            
+          ))}
+        </div> */}
+
+      </Switch>
+
       </div>
     </div>
   )
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 
 import Nav from './ components/Nav';
 import Home from './ components/Home/Home';
 import Shop from './ components/Shop/Shop';
-import Cart from './ components/Cart/Cart';
+import Cart from './ components/Cart/Cart'
 import ItemDetail from './ components/ItemDetail';
 
 function App() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetchShopData()
+  }, [])
+
+  const fetchShopData = async () => {
+    const data = await fetch('https://fakestoreapi.com/products/')
+      .then(response=>response.json(), reason =>console.log(reason))
+    setItems(data)
+  }
+
   return (
     <Router> 
       <div className="App">
         <Nav />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/shop" exact component={Shop} />
-          <Route path="/cart" exact component={Cart}/>
+
+        <Switch>       
+          <Route path="/" exact component={Home} />          
+          <Route path="/shop" exact>
+            <Shop items={items} />
+          </Route>
           <Route path="/shop/:id" component={ItemDetail}/>
+                          
         </Switch>
+
       </div>
     </Router>
   );
